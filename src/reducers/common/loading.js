5aed6854c6aeac5687d6f0bea1fd48e24df16da4
Appian <zhang4baichi@sina.com>
2017-11-16 11:37:32 +0800

    test 2

--- src/reducers/common/loading.js
+++ src/reducers/common/loading.js
@@ -5,8 +5,6 @@ const showLoading = (state = false, action) => {
   switch (action.type) {
     // 根据不用的type来修改state
     case common_type.TOGGLE_LOADING:
-      console.log('---toggle loading');
-      console.log(action);
       return action.value;
     default:
       return state;
