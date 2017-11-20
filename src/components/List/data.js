e613516e467aae81d3e193249f148cf1e2ffc65e
Appian <zhang4baichi@sina.com>
2017-11-20 14:00:37 +0800

    finish

--- src/components/List/data.js
+++ src/components/List/data.js
@@ -1,27 +1,37 @@
 export default [{
   title: '如何造一个『为移动端而生』的日历',
   desc: 'How to Build a Calendar <br/>【算法轮子】【手写类教程】【原生Js】',
-  id:'20160501',
-  url: 'https://ohovav7hg.qnssl.com/articlecover12.png'
+  id:'20170601',
+  url: 'https://ohovav7hg.qnssl.com/articlecover14.png'
+}, {
+  title: 'http 和 https',
+  desc: 'Help You to Understand Http <br/>【手写类教程】【学习笔记】【计算机基础】',
+  id:'20170515',
+  url: 'https://ohovav7hg.qnssl.com/articlecover13.png'
 }, {
   title: '关于数组和字符串的重点方法',
   desc: 'From Zero to Svg <br/>【手写类教程】【学习笔记】',
   id:'20170430',
+  url: 'https://ohovav7hg.qnssl.com/articlecover12.png'
+}, {
+  title: '我在你的手里塞满了糖 ฅ՞•ﻌ•՞ฅ',
+  desc: 'I Love Life <br/>【焦虑小课堂】',
+  id:'20170420',
   url: 'https://ohovav7hg.qnssl.com/articlecover11.png'
 }, {
   title: '从零开始学Svg',
   desc: 'From Zero to Svg <br/>【From Zero系列】【网剧类教程】【学习笔记】',
   id:'20170401',
   url: 'https://ohovav7hg.qnssl.com/articlecover10.png'
-}, {
+}, /* {
   title: '从零开始学正则Reg',
   desc: 'From Zero to Regular <br/>【From Zero系列】【手写类教程】',
   id:'20170313',
   url: 'https://ohovav7hg.qnssl.com/articlecover9.png'
-}, {
+},*/ {
   title: '如何造一个『为移动端而生』的自定义多级联动选择器',
   desc: 'How to Build a Time-picker or Multi-picker<br/>【算法轮子】【手写类教程】【原生Js】',
-  id:'20170103',
+  id:'20170203',
   url: 'https://ohovav7hg.qnssl.com/articlecover8.png'
 }, {
   title: '你应该知道的React事件绑定',
